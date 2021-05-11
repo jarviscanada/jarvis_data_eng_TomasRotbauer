51698651d6d813cc2e922324b13550bc5574efc4
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-05-11 19:54:43 +0000

    Second table added. All done

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,11 +1,27 @@
+--Connect to host_agent database
+\c host_agent;
+
 CREATE TABLE IF NOT EXISTS public.host_info (
-    PRIMARY KEY id SERIAL,
-    hostname VARCHAR UNIQUE NOT NULL,
-    cpu_number SMALLINT NOT NULL,
+    id               SERIAL PRIMARY KEY,
+    hostname         VARCHAR UNIQUE NOT NULL,
+    cpu_number       SMALLINT NOT NULL,
     cpu_architecture VARCHAR NOT NULL,
-    cpu_model VARCHAR NOT NULL,
-    cpu_mhz REAL NOT NULL,
-    L2_cache INTEGER NOT NULL,
-    total_mem INTEGER NOT NULL,
-    "timestamp" TIMESTAMP NOT NULL
+    cpu_model        VARCHAR NOT NULL,
+    cpu_mhz          REAL NOT NULL,
+    L2_cache         INTEGER NOT NULL,
+    total_mem        INTEGER NOT NULL,
+    "timestamp"      TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.host_usage (
+    "timestamp"    TIMESTAMP NOT NULL,
+    host_id        SERIAL NOT NULL,
+    memory_free    INTEGER NOT NULL,
+    cpu_idle       SMALLINT NOT NULL,
+    cpu_kernel     SMALLINT NOT NULL,
+    disk_io        SMALLINT NOT NULL,
+    disk_available INTEGER NOT NULL,
+
+    FOREIGN KEY (host_id) REFERENCES
+        public.host_info (id)
 );
