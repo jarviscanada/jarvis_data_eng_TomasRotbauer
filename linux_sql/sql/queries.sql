bdc078423f3ad3a280d90e298cb10d0e1cffe0e9
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-05-11 22:38:51 +0000

    Work in progress. First query done

---
+++ linux_sql/sql/queries.sql
@@ -0,0 +1,13 @@
+--Group hosts by hardware info
+SELECT cpu_number, id as host_id, total_mem
+FROM public.host_info
+ORDER BY cpu_number, total_mem DESC;
+
+--Average memory usage
+SELECT usage.host_id,
+       info.hostname as host_name,
+       (date_trunc('hour', usage.timestamp) +
+            date_part('minute', usage.timestamp)::int / 5 *
+            interval '5 min') as timestamp,
+       AVG (info.total_mem - usage.memory_free) / info.total_mem * 100
+           as avg_used_mem_percentage
