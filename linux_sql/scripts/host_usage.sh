d0d174284da5cfb325c1e3508ae4f7366a08428e
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-05-13 15:40:15 +0000

    Fixed small bug in call to host_info script

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -2,6 +2,11 @@
 
 USAGE='bash scripts/host_usage.sh psql_host psql_port db_name psql_user psql_password'
 
+get_host_id() {
+    host_id=$(PGPASSWORD="${psql_password}" psql -t -h "${psql_host}" -p "${port}" -d "${db_name}" \
+        -U "${psql_user}" -c "${id_query}")
+}
+
 #validate arguments
 if [ "$#" -ne 5 ]; then
     echo >&2 "Illegal number of parameters"
@@ -23,17 +28,18 @@ top_out=$(top -n 1)
 
 #usage data
 timestamp=$(date --utc "+%Y-%m-%d %T")
-host_id=$(PGPASSWORD="${psql_password}" psql -t -h "${psql_host}" -p "${port}" -d "${db_name}" \
-    -U "${psql_user}" -c "${id_query}")
+get_host_id
 memory_free=$(cat /proc/meminfo | grep -E "^MemFree:" | awk '{print $2}' | xargs)
 cpu_idle=$(echo "$top_out" | grep -E "^%Cpu\(s\):" | awk '{print $8}' | xargs)
 cpu_kernel=$(echo "$top_out" | grep -E "^%Cpu\(s\):" | awk '{print $4}' | xargs)
 disk_io=$(vmstat -d | grep -E "^sda" | awk '{print $10}' | xargs)
 disk_available=$(df -BM / | grep -E "^/dev/sda2" | awk '{print $4}' | xargs)
 
+#if host not in host_info table yet, must add first.
 if [ -z "$host_id" ]; then
-  echo >&2 "Host was not found in host_info table. No insertions to host_usage were made."
-  exit 1
+    /home/centos/dev/jarvis_data_eng_Tomas/linux_sql/scripts/host_info.sh \
+        "${psql_host}" "${port}" "${db_name}" "${psql_user}" "${psql_password}";
+    get_host_id;
 fi
 
 insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, \
