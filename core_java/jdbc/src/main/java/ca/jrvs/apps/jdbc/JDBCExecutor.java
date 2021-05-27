f8596028b221c8c701f5d4230ccedc82de5ccd1f
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-05-27 22:11:45 +0000

    Created database connection manager

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,9 +1,33 @@
 package ca.jrvs.apps.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JDBCExecutor {
+  final Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
 
   public static void main(String[] args) {
-    System.out.println("Hello Learning JDBC");
-  }
+    BasicConfigurator.configure();
+    JDBCExecutor jdbcExecutor = new JDBCExecutor();
 
+    DatabaseConnectionManager dcm = new DatabaseConnectionManager (
+        "localhost", "hplussport",
+        "postgres", "password");
+
+    try {
+      Connection connection = dcm.getConnection();
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM customer;");
+      while (resultSet.next()) {
+        jdbcExecutor.logger.info(String.valueOf(resultSet.getInt(1)));
+      }
+    } catch (SQLException ex) {
+      jdbcExecutor.logger.error(ex.getMessage());
+    }
+  }
 }
