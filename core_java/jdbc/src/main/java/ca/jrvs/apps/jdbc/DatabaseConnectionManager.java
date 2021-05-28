a64600fe4b26dedd854adae9018d21423856049d
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-05-28 16:33:05 +0000

    Completed Data access - creating data

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DatabaseConnectionManager.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/DatabaseConnectionManager.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnectionManager {
+
   private final String url;
   private final Properties properties;
 
   public DatabaseConnectionManager(String host, String databaseName,
-                                   String username, String password) {
-    this.url = "jdbc:postgresql://"+host+"/"+databaseName;
+      String username, String password) {
+    this.url = "jdbc:postgresql://" + host + "/" + databaseName;
     this.properties = new Properties();
     this.properties.setProperty("user", username);
     this.properties.setProperty("password", password);
--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,31 +1,37 @@
 package ca.jrvs.apps.jdbc;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JDBCExecutor {
+
   final Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
 
   public static void main(String[] args) {
     BasicConfigurator.configure();
     JDBCExecutor jdbcExecutor = new JDBCExecutor();
 
-    DatabaseConnectionManager dcm = new DatabaseConnectionManager (
+    DatabaseConnectionManager dcm = new DatabaseConnectionManager(
         "localhost", "hplussport",
         "postgres", "password");
 
     try {
       Connection connection = dcm.getConnection();
-      Statement statement = connection.createStatement();
-      ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM customer;");
-      while (resultSet.next()) {
-        jdbcExecutor.logger.info(String.valueOf(resultSet.getInt(1)));
-      }
+      CustomerDAO customerDAO = new CustomerDAO(connection);
+      Customer customer = new Customer();
+      customer.setFirstName("Larry");
+      customer.setLastName("Lobster");
+      customer.setEmail("larry.lobster@goolagoon.com");
+      customer.setPhone("123-456-7890");
+      customer.setAddress("Goo Lagoon Gym");
+      customer.setCity("Bikini Bottom");
+      customer.setState("Under The Sea");
+      customer.setZipCode("12345");
+
+      customerDAO.create(customer);
     } catch (SQLException ex) {
       jdbcExecutor.logger.error(ex.getMessage());
     }
