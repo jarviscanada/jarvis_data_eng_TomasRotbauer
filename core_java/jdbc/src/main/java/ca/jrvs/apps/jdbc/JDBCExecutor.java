524a522d21c930f941895cf206f45e9c57b7d0b5
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-05-28 23:59:44 +0000

    Finished Using stored procedures

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -2,6 +2,7 @@ package ca.jrvs.apps.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,8 +22,8 @@ public class JDBCExecutor {
     try {
       Connection connection = dcm.getConnection();
       OrderDAO orderDAO = new OrderDAO(connection);
-      Order order = orderDAO.findById(1000);
-      jdbcExecutor.logger.info(order.toString());
+      List<Order> orders = orderDAO.getOrdersForCustomer(789);
+      orders.forEach(o -> jdbcExecutor.logger.info(o.toString()));
     } catch (SQLException ex) {
       jdbcExecutor.logger.error(ex.getMessage());
     }
--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/Order.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/Order.java
@@ -1,16 +1,80 @@
 package ca.jrvs.apps.jdbc;
 
 import ca.jrvs.apps.jdbc.util.DataTransferObject;
-import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
 public class Order implements DataTransferObject {
 
   private long id;
-  private Timestamp creationDate;
+  private Date creationDate;
   private double totalDue;
   private String status;
   private long customerId;
   private long salespersonId;
+  private String customerFirstName;
+  private String customerLastName;
+  private String customerEmail;
+  private String salespersonFirstName;
+  private String salespersonLastName;
+  private String salespersonEmail;
+  private List<OrderLine> orderLines;
+
+  public List<OrderLine> getOrderLines() {
+    return orderLines;
+  }
+
+  public void setOrderLines(List<OrderLine> orderLines) {
+    this.orderLines = orderLines;
+  }
+
+  public String getCustomerFirstName() {
+    return customerFirstName;
+  }
+
+  public void setCustomerFirstName(String customerFirstName) {
+    this.customerFirstName = customerFirstName;
+  }
+
+  public String getCustomerLastName() {
+    return customerLastName;
+  }
+
+  public void setCustomerLastName(String customerLastName) {
+    this.customerLastName = customerLastName;
+  }
+
+  public String getCustomerEmail() {
+    return customerEmail;
+  }
+
+  public void setCustomerEmail(String customerEmail) {
+    this.customerEmail = customerEmail;
+  }
+
+  public String getSalespersonFirstName() {
+    return salespersonFirstName;
+  }
+
+  public void setSalespersonFirstName(String salespersonFirstName) {
+    this.salespersonFirstName = salespersonFirstName;
+  }
+
+  public String getSalespersonLastName() {
+    return salespersonLastName;
+  }
+
+  public void setSalespersonLastName(String salespersonLastName) {
+    this.salespersonLastName = salespersonLastName;
+  }
+
+  public String getSalespersonEmail() {
+    return salespersonEmail;
+  }
+
+  public void setSalespersonEmail(String salespersonEmail) {
+    this.salespersonEmail = salespersonEmail;
+  }
 
   @Override
   public long getId() {
@@ -21,11 +85,11 @@ public class Order implements DataTransferObject {
     this.id = id;
   }
 
-  public Timestamp getCreationDate() {
+  public Date getCreationDate() {
     return creationDate;
   }
 
-  public void setCreationDate(Timestamp creationDate) {
+  public void setCreationDate(Date creationDate) {
     this.creationDate = creationDate;
   }
 
@@ -70,6 +134,13 @@ public class Order implements DataTransferObject {
         ", status='" + status + '\'' +
         ", customerId=" + customerId +
         ", salespersonId=" + salespersonId +
+        ", customerFirstName='" + customerFirstName + '\'' +
+        ", customerLastName='" + customerLastName + '\'' +
+        ", customerEmail='" + customerEmail + '\'' +
+        ", salespersonFirstName='" + salespersonFirstName + '\'' +
+        ", salespersonLastName='" + salespersonLastName + '\'' +
+        ", salespersonEmail='" + salespersonEmail + '\'' +
+        ", orderLines=" + orderLines +
         '}';
   }
 }
