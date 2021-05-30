838a82fa76f4b4ab89ae2ccc722120c493094366
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-05-30 21:44:48 +0000

    Compare maps aproach 1 + tests

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/CompareMaps.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/CompareMaps.java
@@ -1,12 +1,18 @@
 package ca.jrvs.practice.codingChallenge;
 
+import java.util.Map;
+
 /**
  * Ticket link: https://www.notion.so/jarvisdev/How-to-compare-two-maps-8d439bbb85db4b99ba2cdc183509a5db
  */
 public class CompareMaps {
 
   /**
-   * Complexity: O(n)
+   * Complexity: O(n), n ~ the number of pairs in both maps
+   * Justification: Need to ensure one maps' items are in the other and vice versa
    */
+  public <K,V> boolean compareMaps(Map<K,V> m1, Map<K,V> m2) {
+    return m1.equals(m2);
+  }
 
 }
---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/CompareMapsTest.java
@@ -0,0 +1,50 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CompareMapsTest {
+
+  private final Map<Integer, Integer> m1 = new HashMap<>();
+  private final Map<Integer, Integer> m2 = new HashMap<>();
+  private final CompareMaps compareMaps = new CompareMaps();
+
+  @Before
+  public void setUp() {
+    int i = 0;
+    int j = 10;
+
+    while (i < 10) {
+      m1.put(i, j);
+      m2.put(i, j);
+      i++;
+      j--;
+    }
+  }
+
+  @After
+  public void cleanUp() {
+    m1.clear();
+    m2.clear();
+  }
+
+  @Test
+  public void testEqual() {
+    setUp();
+    Assert.assertTrue(compareMaps.compareMaps(m1, m2));
+    cleanUp();
+    }
+
+  @Test
+  public void testNotEqual() {
+    setUp();
+    m1.put(-1,-1);
+    Assert.assertFalse(compareMaps.compareMaps(m1, m2));
+    cleanUp();
+  }
+
+}
