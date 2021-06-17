2fc06243ccafe66544bb084f9f8018390970f072
Tomas Rotbauer <tomasrotbauer907@gmail.com>
2021-06-17 18:46:29 +0000

    Finished Twitter app implementation

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCLIApp.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/TwitterCLIApp.java
@@ -1,27 +1,68 @@
 package ca.jrvs.apps.twitter;
 
+import ca.jrvs.apps.twitter.controller.Controller;
+import ca.jrvs.apps.twitter.controller.TwitterController;
+import ca.jrvs.apps.twitter.dao.CrdDao;
+import ca.jrvs.apps.twitter.dao.TwitterDao;
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
 import ca.jrvs.apps.twitter.dao.helper.TwitterHttpHelper;
-import com.google.gdata.util.common.base.PercentEscaper;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
+import ca.jrvs.apps.twitter.example.JsonParser;
+import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.service.Service;
+import ca.jrvs.apps.twitter.service.TwitterService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TwitterCLIApp {
+  public static final String USAGE = "Usage: TwitterCLIApp <post | show | delete> [options]";
 
   private static final String CONSUMER_KEY = System.getenv("consumerKey");
   private static final String CONSUMER_SECRET = System.getenv("consumerSecret");
   private static final String ACCESS_TOKEN = System.getenv("accessToken");
   private static final String TOKEN_SECRET = System.getenv("tokenSecret");
 
-  public static void main(String[] args) throws URISyntaxException, IOException {
-    TwitterHttpHelper httpHelper = new TwitterHttpHelper(CONSUMER_KEY, CONSUMER_SECRET,
+  private static Controller controller;
+
+  public static void main(String[] args) throws IllegalArgumentException {
+    TwitterCLIApp app = new TwitterCLIApp();
+
+    HttpHelper httpHelper = new TwitterHttpHelper(CONSUMER_KEY, CONSUMER_SECRET,
         ACCESS_TOKEN, TOKEN_SECRET);
+    CrdDao<Tweet, String> dao = new TwitterDao(httpHelper);
+    Service service = new TwitterService(dao);
+    controller = new TwitterController(service);
 
-    URI uri = new URI("https://api.twitter.com/1.1/statuses/show.json?id=1404474031647215621");
+    app.run(args);
+  }
 
-    HttpResponse response = httpHelper.httpGet(uri);
-    System.out.println(EntityUtils.toString(response.getEntity()));
+  public void run(String[] args) {
+    if (args.length == 0)
+      throw new IllegalArgumentException(USAGE);
+
+    String command = args[0];
+    List<Tweet> response;
+
+    if (command.equals("post")) {
+      response = new ArrayList<>();
+      response.add(controller.postTweet(args));
+    }
+    else if (command.equals("show")) {
+      response = new ArrayList<>();
+      response.add(controller.showTweet(args));
+    }
+    else if (command.equals("delete"))
+      response = controller.deleteTweet(args);
+    else
+      throw new IllegalArgumentException(USAGE);
+
+    response.forEach(t -> {
+      try {
+        System.out.println(JsonParser.toJson(t, true, true));
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+    });
+
   }
 }
